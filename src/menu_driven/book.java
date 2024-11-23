9510753a2d7e9d5549c2c5ead47c76cdb0148243
Atharva <jatharva70@gmai.com>
2024-11-23 19:09:03 +0530

    iu7i

--- src/menu_driven/book.java
+++ src/menu_driven/book.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class book {
 
 	private static void addBook(Connection con, Scanner sc) throws SQLException {
-	
+
 		System.out.println("Enter Book name: ");
 		String name = sc.nextLine();
 		System.out.println("Enter Book author: ");
@@ -23,86 +23,87 @@ public class book {
 			pstmt.setString(1, name);
 			pstmt.setString(2, author);
 			pstmt.setFloat(3, price);
-			
+
 			int rows = pstmt.executeUpdate();
 			System.out.println(rows > 0 ? "Book added successfully!" : "Failed to add book");
 		}
 	}
-	
+
 	private static void removeBook(Connection con, Scanner sc) throws SQLException {
 		System.out.println("Enter book ID to remove: ");
 		int id = sc.nextInt();
-		
+
 		String sql = "DELETE FROM book where id = ?";
-		try(PreparedStatement pstmt = con.prepareStatement(sql)){
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
 			pstmt.setInt(1, id);
 			int rows = pstmt.executeUpdate();
 			System.out.println(rows > 0 ? "Book removed successfully" : "book not found.");
 		}
 	}
-	
+
 	private static void updateBookName(Connection con, Scanner sc) throws SQLException {
 		System.out.println("Enter Book ID to Upadte: ");
 		int id = sc.nextInt();
 		sc.nextLine();
 		System.out.println("Enter a new book name: ");
 		String newName = sc.nextLine();
-		
+
 		String sql = "UPDATE book SET name = ? WHERE id = ?";
-		try(PreparedStatement pstmt = con.prepareStatement(sql)){
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
 			pstmt.setString(1, newName);
 			pstmt.setInt(2, id);
 			int rows = pstmt.executeUpdate();
-			System.out.println(rows > 0 ? "Book name update successfully":"Book not found.");
+			System.out.println(rows > 0 ? "Book name update successfully" : "Book not found.");
 		}
 	}
-	
+
 	private static void updateBookAuthor(Connection con, Scanner sc) throws SQLException {
 		System.out.println("Enter Book ID to Upadte: ");
 		int id = sc.nextInt();
 		sc.nextLine();
 		System.out.println("Enter a new author: ");
 		String newAuthor = sc.nextLine();
-		
+
 		String sql = "UPDATE book SET author = ? WHERE id = ?";
-		try(PreparedStatement pstmt = con.prepareStatement(sql)){
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
 			pstmt.setString(1, newAuthor);
 			pstmt.setInt(2, id);
 			int rows = pstmt.executeUpdate();
-			System.out.println(rows > 0 ? "Book author update successfully":"Book not found.");
+			System.out.println(rows > 0 ? "Book author update successfully" : "Book not found.");
 		}
 	}
-	
+
 	private static void updateBookPrice(Connection con, Scanner sc) throws SQLException {
 		System.out.println("Enter Book ID to Upadte: ");
 		int id = sc.nextInt();
 		sc.nextLine();
 		System.out.println("Enter a new price: ");
 		float newPrice = sc.nextFloat();
-		
+
 		String sql = "UPDATE book SET price = ? WHERE id = ?";
-		try(PreparedStatement pstmt = con.prepareStatement(sql)){
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
 			pstmt.setFloat(1, newPrice);
 			pstmt.setInt(2, id);
 			int rows = pstmt.executeUpdate();
-			System.out.println(rows > 0 ? "Book price update successfully":"Book not found.");
+			System.out.println(rows > 0 ? "Book price update successfully" : "Book not found.");
 		}
 	}
-	
+
 	private static void searchBookByName(Connection con, Scanner sc) throws SQLException {
 		System.out.println("Enter a book name to search: ");
 		String name = sc.nextLine();
-		
+
 		String sql = "SELECT * FROM book WHERE name LIKE ?";
-		try(PreparedStatement pstmt = con.prepareStatement(sql)){
-			pstmt.setString(1,"%" + name + "%");
-			try(ResultSet rs = pstmt.executeQuery()){
-				if(!rs.next()) {
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setString(1, "%" + name + "%");
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (!rs.next()) {
 					System.out.println("No Book found with the given name.");
 				} else {
 					do {
-						System.out.printf("ID: %d, Name: %s, Author: %s, Price: %.2f%n", rs.getInt("id"),rs.getString("name"),rs.getString("author"),rs.getDouble("price"));
-					}while(rs.next());
+						System.out.printf("ID: %d, Name: %s, Author: %s, Price: %.2f%n", rs.getInt("id"),
+								rs.getString("name"), rs.getString("author"), rs.getDouble("price"));
+					} while (rs.next());
 				}
 			}
 		}
@@ -130,17 +131,23 @@ public class book {
 
 				switch (choice) {
 				case 1:
-					addBook(con, sc); break;
+					addBook(con, sc);
+					break;
 				case 2:
-					removeBook(con, sc); break;
+					removeBook(con, sc);
+					break;
 				case 3:
-					updateBookName(con, sc); break;
+					updateBookName(con, sc);
+					break;
 				case 4:
-					updateBookAuthor(con, sc); break;
+					updateBookAuthor(con, sc);
+					break;
 				case 5:
-					updateBookPrice(con, sc); break;
+					updateBookPrice(con, sc);
+					break;
 				case 6:
-					searchBookByName(con, sc); break;
+					searchBookByName(con, sc);
+					break;
 				case 7: {
 					System.out.println("Exiting...");
 					return;
